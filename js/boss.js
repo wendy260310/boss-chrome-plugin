bc1c3b99c44bdc30f7ea91a6a381d84e2e2a046b
linlan.zcj <linlan.zcj@alibaba-inc.com>
2018-12-08 23:11:27 +0800

    final commit

--- js/boss.js
+++ js/boss.js
@@ -2,7 +2,7 @@
     //get job id
     let jobId;
     let message;
-
+    let config = undefined;
     let waiter = function (intervel, breakCondition, doAfterBreak) {
         if (!intervel)
             intervel = 1000;
@@ -16,6 +16,16 @@
             }
         }
     }
+
+    let loadConfig = function () {
+        let keyArray = ['salary', 'experience', 'degree', 'max-age', 'min-exp', 'max-exp'
+            , 'min-salary', 'job-index', 'message-index', 'page-count'];
+
+        chrome.storage.local.get(keyArray, function (result) {
+            config = result;
+        });
+    }
+
 
     let getJob = function (i) {
         let jobRequest = new XMLHttpRequest();
@@ -41,7 +51,7 @@
 
         r.onreadystatechange = () => {
             if (r.readyState == XMLHttpRequest.DONE && r.status == 200) {
-                message = JSON.parse(r.response).replyWords[2];
+                message = JSON.parse(r.response).replyWords[config['message-index'] - 1];
             }
         }
         r.open('GET', url, true);
@@ -50,17 +60,20 @@
 
 
     let filter = function (greetAnchor) {
-        let maxAge = 30;
-        let maxWorkAge = 5;
-        let minWorkAge = 3;
-        let minSalary = 23;
-
+        let maxAge = config['max-age'];
+        let maxWorkAge = config['max-exp'];
+        let minWorkAge = config['min-exp'];
+        let minSalary = config['min-salary'];
 
         //取到现在的公司
         let exp = greetAnchor.getElementsByClassName('experience')[0];
-        let skipCompany = ['阿里巴巴', '高德', '饿了么', '菜鸟', '苏宁', '飞猪', '天猫']
+        let skipCompanyStr = config['company'];
+        let skipCompany;
+        if (skipCompanyStr) {
+            skipCompany = JSON.parse(skipCompanyStr);
+        }
 
-        if (exp) {
+        if (exp && skipCompany) {
             for (let c in skipCompany) {
                 if (exp.innerText.indexOf(skipCompany[c]) > -1) {
                     return false;
@@ -81,106 +94,115 @@
         let currentSalaryStr = currentSalarySpan.split("-")[0];
         let currentSalary = currentSalaryStr.substr(0, currentSalaryStr.length - 1);
 
-        return maxAge >= currentAge && minWorkAge <= workAge
-            && maxWorkAge >= workAge && currentSalary >= minSalary;
+        return (!maxAge || maxAge >= currentAge) && (!minWorkAge || minWorkAge <= workAge)
+            && (!maxWorkAge || maxWorkAge >= workAge) && (!minSalary || currentSalary >= minSalary);
 
     }
 
-    getJob(0);
-    waiter(500, () => jobId, () => {
-        getReply();
-        let greetArray = [];
-        let totalPage = 3;
-        let finishPage = 0;
-        for (let m = 1; m <= totalPage; ++m) {
-            let getGeekHttpRequest = new XMLHttpRequest();
-            let geekUrl = 'https://www.zhipin.com/boss/recommend/geeks.json?status=0&jobid=' + jobId
-                + '&salary=406&experience=105&degree=203&_=1543914502205&page=' + m;
-            getGeekHttpRequest.onreadystatechange = function () {
-                if (getGeekHttpRequest.readyState == XMLHttpRequest.DONE
-                    && getGeekHttpRequest.status == 200) {
-                    let response = JSON.parse(getGeekHttpRequest.response);
-                    if (response.hasMore) {
-                        let doc = document.createElement("div");
-                        doc.innerHTML = response.htmlList;
-                        //取到所有的待打招呼的节点
-                        let greet = doc.getElementsByClassName('btn-greet');
-                        for (let n = 0; n < greet.length; ++n) {
-                            let a = greet[n].closest('.sider-op').nextElementSibling;
-                            if (!filter(a)) {
-                                continue;
+    loadConfig();
+    waiter(500, () => config, () => {
+        getJob(config['job-index'] - 1);
+        waiter(500, () => jobId, () => {
+            getReply();
+            let greetArray = [];
+            let totalPage = config['page-count']
+            if (!totalPage) {
+                alert("没有设置候选人页数，默认是1");
+                totalPage = 1;
+            }
+
+            let finishPage = 0;
+            for (let m = 1; m <= totalPage; ++m) {
+                let getGeekHttpRequest = new XMLHttpRequest();
+                let geekUrl = 'https://www.zhipin.com/boss/recommend/geeks.json?status=0&jobid=' + jobId
+                    + '&salary=' + (config['salary'] || 0) + '&experience=' + (config['experience'] || 0) +
+                    '&degree=' + (config['degree'] || 0) + '&_=1543914502205&page=' + m;
+                getGeekHttpRequest.onreadystatechange = function () {
+                    if (getGeekHttpRequest.readyState == XMLHttpRequest.DONE
+                        && getGeekHttpRequest.status == 200) {
+                        let response = JSON.parse(getGeekHttpRequest.response);
+                        if (response.hasMore) {
+                            let doc = document.createElement("div");
+                            doc.innerHTML = response.htmlList;
+                            //取到所有的待打招呼的节点
+                            let greet = doc.getElementsByClassName('btn-greet');
+                            for (let n = 0; n < greet.length; ++n) {
+                                let a = greet[n].closest('.sider-op').nextElementSibling;
+                                if (!filter(a)) {
+                                    continue;
+                                }
+                                greetArray.push(a);
                             }
-                            greetArray.push(a);
+                            finishPage++;
                         }
-                        finishPage++;
                     }
                 }
+                getGeekHttpRequest.open("GET", geekUrl, true);
+                getGeekHttpRequest.send();
             }
-            getGeekHttpRequest.open("GET", geekUrl, true);
-            getGeekHttpRequest.send();
-        }
-        waiter(500, () => finishPage == totalPage, () => {
-            let btnGreetUrl = "https://www.zhipin.com/chat/batchAddRelation.json";
-            //记录下多少人成功的打招呼了
-            let btnGreetSuccessNum = 0;
-            //网络请求完成了多少个
-            let btnGreetFinish = 0;
-            for (let i = 0; i < greetArray.length; ++i) {
-                let a = greetArray[i];
-                let param = 'gids=' + a.dataset.uid + "&jids=" + a.dataset.jid
-                    + '&expectIds=' + a.dataset.expect + "&lids=" + a.dataset.lid;
+            waiter(500, () => finishPage == totalPage, () => {
+                let btnGreetUrl = "https://www.zhipin.com/chat/batchAddRelation.json";
+                //记录下多少人成功的打招呼了
+                let btnGreetSuccessNum = 0;
+                //网络请求完成了多少个
+                let btnGreetFinish = 0;
+                for (let i = 0; i < greetArray.length; ++i) {
+                    let a = greetArray[i];
+                    let param = 'gids=' + a.dataset.uid + "&jids=" + a.dataset.jid
+                        + '&expectIds=' + a.dataset.expect + "&lids=" + a.dataset.lid;
 
 
-                let btnGreetHttpRequest = new XMLHttpRequest();
+                    let btnGreetHttpRequest = new XMLHttpRequest();
 
-                btnGreetHttpRequest.open("POST", btnGreetUrl, false);
-                btnGreetHttpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-                btnGreetHttpRequest.onreadystatechange = () => {
-                    if (btnGreetHttpRequest.readyState == XMLHttpRequest.DONE &&
-                        btnGreetHttpRequest.status == 200) {
-                        let btnGreetResponse = JSON.parse(btnGreetHttpRequest.response);
-                        if (btnGreetResponse.rescode == 1) {
-                            btnGreetSuccessNum++;
+                    btnGreetHttpRequest.open("POST", btnGreetUrl, false);
+                    btnGreetHttpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+                    btnGreetHttpRequest.onreadystatechange = () => {
+                        if (btnGreetHttpRequest.readyState == XMLHttpRequest.DONE &&
+                            btnGreetHttpRequest.status == 200) {
+                            let btnGreetResponse = JSON.parse(btnGreetHttpRequest.response);
+                            if (btnGreetResponse.rescode == 1) {
+                                btnGreetSuccessNum++;
+                            }
+                            btnGreetFinish++;
                         }
-                        btnGreetFinish++;
                     }
+                    btnGreetHttpRequest.send(param);
                 }
-                btnGreetHttpRequest.send(param);
-            }
 
-            waiter(500, () => greetArray.length == btnGreetFinish, () => {
+                waiter(500, () => greetArray.length == btnGreetFinish, () => {
 
-                let communicate = document.querySelectorAll("[ka='menu-im']")[0];
-                communicate.click();
-                //找到chat room
-                let chatContainer = document.getElementsByClassName('chat-container')[0];
-                let chatMessage = chatContainer.getElementsByClassName('chat-message')[0];
-                let btnSend = chatContainer.getElementsByClassName('btn-send')[0];
+                    let communicate = document.querySelectorAll("[ka='menu-im']")[0];
+                    communicate.click();
+                    //找到chat room
+                    let chatContainer = document.getElementsByClassName('chat-container')[0];
+                    let chatMessage = chatContainer.getElementsByClassName('chat-message')[0];
+                    let btnSend = chatContainer.getElementsByClassName('btn-send')[0];
 
-                waiter(500, () => chatContainer.style.display === 'block'
-                    , () => {
-                        //进到聊天了，然后要点开具体的某个人
-                        let mainList = document.getElementsByClassName('main-list')[0]
-                            .getElementsByTagName('li');
+                    waiter(500, () => chatContainer.style.display === 'block'
+                        , () => {
+                            //进到聊天了，然后要点开具体的某个人
+                            let mainList = document.getElementsByClassName('main-list')[0]
+                                .getElementsByTagName('li');
 
-                        sendMessageOneByOne(0, btnGreetSuccessNum, mainList, chatMessage, btnSend);
-                    });
-
-                function sendMessageOneByOne(curIndex, maxIndex, mainList, chatMessage, btnSend) {
-                    if (curIndex >= maxIndex)
-                        return;
-                    let li = mainList[curIndex];
-                    li.getElementsByTagName('a')[0].click();
-                    waiter(500, () => Object.values(li.classList).indexOf("cur") > -1,
-                        () => {
-                            chatMessage.textContent = message;
-                            btnSend.click();
-                            setTimeout(() => {
-                            }, 1000);
-                            sendMessageOneByOne(curIndex + 1, maxIndex, mainList, chatMessage, btnSend);
+                            sendMessageOneByOne(0, btnGreetSuccessNum, mainList, chatMessage, btnSend);
                         });
-                }
-            })
+
+                    function sendMessageOneByOne(curIndex, maxIndex, mainList, chatMessage, btnSend) {
+                        if (curIndex >= maxIndex)
+                            return;
+                        let li = mainList[curIndex];
+                        li.getElementsByTagName('a')[0].click();
+                        waiter(500, () => Object.values(li.classList).indexOf("cur") > -1,
+                            () => {
+                                chatMessage.textContent = message;
+                                btnSend.click();
+                                setTimeout(() => {
+                                }, 1000);
+                                sendMessageOneByOne(curIndex + 1, maxIndex, mainList, chatMessage, btnSend);
+                            });
+                    }
+                })
+            });
         });
     });
 })();
--- js/popup.js
+++ js/popup.js
@@ -5,10 +5,65 @@
             file: '/js/boss.js'
         });
     });
+    //radio config
+    let salary = document.getElementById("salary");
+    let experience = document.getElementById("experience");
+    let degree = document.getElementById("degree");
+    let radioConfig = [salary, experience, degree];
+    for (let index in radioConfig) {
+        //添加事件
+        radioConfig[index].addEventListener('click', (event) => saveBossConfig(event))
+        //加载之前的配置
+        let keyId = radioConfig[index].dataset.keyid;
+        chrome.storage.local.get([keyId], function (result) {
+            if (result && result[keyId]) {
+                loadRadioConfig(result[keyId], radioConfig[index]);
+            }
+        });
+    }
+    let filter = document.getElementById("filter");
+    let basicConfig = document.getElementById("basic-config");
 
-    document.getElementById("main-panel").addEventListener('click', function (e) {
-        console.log(e.target);
-    });
+    let inputConfig = [filter, basicConfig];
+    for (let key in inputConfig) {
+        inputConfig[key].addEventListener("change", (event) => saveInputConfig(event));
+        loadInputConfig(inputConfig[key]);
+    }
+
+    function saveBossConfig(e) {
+        let keyId = e.currentTarget.dataset.keyid;
+        let keyValue = e.target.dataset.keyvalue;
+        chrome.storage.local.set({[keyId]: keyValue}, undefined);
+    }
+
+    function saveInputConfig(e) {
+        let keyId = e.target.dataset.keyid;
+        let keyValue = e.target.value;
+        chrome.storage.local.set({[keyId]: keyValue}, undefined);
+    }
+
+
+    function loadRadioConfig(storeValue, dom) {
+        let radios = dom.getElementsByTagName('input');
+        for (let i = 0; i < radios.length; ++i) {
+            if (radios[i].dataset.keyvalue === storeValue) {
+                radios[i].checked = true;
+                break;
+            }
+        }
+    }
+
+    function loadInputConfig(dom) {
+        let input = dom.getElementsByTagName('input');
+        for (let i = 0; i < input.length; ++i) {
+            let key = input[i].dataset.keyid;
+            chrome.storage.local.get([key], function (result) {
+                if (result && result[key]) {
+                    input[i].value = result[key];
+                }
+            })
+        }
+    }
 
 })();
 
